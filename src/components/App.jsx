069922877c46cb5024d3d28a6b83ebd664d655ff
Mark <mgnorrish@gmail.com>
2022-10-09 20:32:40 +1100

    refactor, show reagent colors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import Plant from "./Plant";
 import PlantFilter from "./PlantFilter";
 // import Skill from "./Skill_filter";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const plants = require("../data/flora.json")
 
@@ -16,14 +15,15 @@ function App() {
   return (
     <div className="App">
       <header>
-        <p>
-          Request data about Hezulim flora, aka paikera.
-        </p>
+        <h1>
+          Request data about Hezulim flora.
+        </h1>
       </header>
         
       <PlantFilter ixUpdate={setAnimateFilterIx} name="animate vine" />
       {/* TODO concat everything in plants.keys or something? */}
       {/* TODO it would be nice if these didn't all have to be level: when one Plant has elements on multiple lines and thus has more height, it shouldn't push the entire next line down, only the one directly under it */}
+      {/* TODO we should really show a plant's colours. Ideally we'd set the text colour to match. */}
       <div className="row">
         {(plants.actives.concat(plants.ampers, plants.dampers, plants.preservatives)).filter(filters[animateFilterIx]).map((data, ix) => <Plant key={ix} colors={data.colors} description={data.description} effect={data.effect} name={data.name} tags={(data.tags && data.tags.join(", ")) || ""} />)}
       </div>
--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-
-function colorsFromString(s) {
+function colorFromChar(c) {
   const lookup = {"R":"red", "O": "orange", "Y":"yellow", "G":"green", "B":"blue", "P":"purple"};
-  return lookup[s[0]] + ", " + lookup[s[1]];
+  return lookup[c]
+}
+
+function displayColor(s) {
+  const lookup = {"yellow":"orange", "orange":"brown"};
+  return lookup[s] ? lookup[s] : s;
+}
+
+function styledColorsFromString(s) {
+  const firstColor = colorFromChar(s[0]);
+  const secondColor = colorFromChar(s[1]);
+  return <p>
+    <span style={{color:displayColor(firstColor)}}>{firstColor}</span>, <span style={{color:displayColor(secondColor)}}>{secondColor}</span>
+  </p>
 }
 
 // TODO this should show the reagent type somewhere
@@ -13,9 +25,7 @@ function Plant(props) {
       <h3>
         {props.name}
       </h3>
-      <p>
-        {colorsFromString(props.colors)}
-      </p>
+      {styledColorsFromString(props.colors)}
       <p>
         {props.effect}
       </p>
