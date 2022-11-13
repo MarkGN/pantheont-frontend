40dd8cd62909661e5ee9514cfd07b820de82bf78
Mark <mgnorrish@gmail.com>
2022-11-14 09:41:23 +1100

    prettify legend

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const data = require("../data/legend.json");
 
 export default function Legend(props) {
-  return <div className="legend">
+  return <div className="legend"><div className="inner-legend">
     {(data[props.contentType] || [""]).map((line, ix) => <p key={ix}>{line}</p>)}
-  </div>
+  </div></div>
 }
